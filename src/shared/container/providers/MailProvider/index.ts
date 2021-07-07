dca64757586afb4a52773d1dc25aadf01f44a9a2
João Pedro <joaopedrocj@gmail.com>
2021-07-07 17:53:36 -0300

    fix: postmark dependencies

--- src/shared/container/providers/MailProvider/index.ts
+++ src/shared/container/providers/MailProvider/index.ts
@@ -5,11 +5,13 @@ import IMailProvider from './models/IMailProvider';
 
 import EtherealMailProvider from './implementations/EtherealMailProvider';
 import SESMailProvider from './implementations/SESMailProvider';
+import PostmarkProvider from './implementations/PostmarkProvider';
 
 const providers = {
   ethereal: container.resolve(EtherealMailProvider),
-  ses: container.resolve(SESMailProvider)
-}
+  ses: container.resolve(SESMailProvider),
+  postmark: container.resolve(PostmarkProvider),
+};
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
